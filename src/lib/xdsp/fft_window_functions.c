fd51884a3edde357b46233a28bec1129d2f9eae2
Yuri Nikolaev <irodushka@gmail.com>
2025-11-08 21:53:39 +0300

    add AVX512 funcs for fft windows & i16-f32

--- src/lib/xdsp/fft_window_functions.c
+++ src/lib/xdsp/fft_window_functions.c
@@ -13,6 +13,13 @@ VWLT_ATTRIBUTE(optimize("-O3"), target("avx2"))
 DECLARE_TR_FUNC_FFT_WINDOW_CF32(fft_window_cf32_avx2)
 #endif
 
+#ifdef WVLT_AVX512BW
+#define TEMPLATE_FUNC_NAME fft_window_cf32_avx512bw
+VWLT_ATTRIBUTE(optimize("-O3"), target("avx512bw"))
+#include "templates/fft_window_cf32_avx512bw.t"
+DECLARE_TR_FUNC_FFT_WINDOW_CF32(fft_window_cf32_avx512bw)
+#endif
+
 #define TEMPLATE_FUNC_NAME fft_window_ci16_cf32_generic
 VWLT_ATTRIBUTE(optimize("-O3"))
 #include "templates/fft_window_ci16_cf32_generic.t"
@@ -25,6 +32,13 @@ VWLT_ATTRIBUTE(optimize("-O3"), target("avx2"))
 DECLARE_TR_FUNC_FFT_WINDOW_CI16_CF32(fft_window_ci16_cf32_avx2)
 #endif
 
+#ifdef WVLT_AVX512BW
+#define TEMPLATE_FUNC_NAME fft_window_ci16_cf32_avx512bw
+VWLT_ATTRIBUTE(optimize("-O3"), target("avx512bw"))
+#include "templates/fft_window_ci16_cf32_avx512bw.t"
+DECLARE_TR_FUNC_FFT_WINDOW_CI16_CF32(fft_window_ci16_cf32_avx512bw)
+#endif
+
 fft_window_cf32_function_t fft_window_cf32_c(generic_opts_t cpu_cap, const char** sfunc)
 {
     const char* fname;
@@ -32,6 +46,7 @@ fft_window_cf32_function_t fft_window_cf32_c(generic_opts_t cpu_cap, const char*
 
     SELECT_GENERIC_FN(fn, fname, tr_fft_window_cf32_generic, cpu_cap);
     SELECT_AVX2_FN(fn, fname, tr_fft_window_cf32_avx2, cpu_cap);
+    SELECT_AVX512BW_FN(fn, fname, tr_fft_window_cf32_avx512bw, cpu_cap);
 
     if (sfunc) *sfunc = fname;
     return fn;
@@ -44,6 +59,7 @@ fft_window_ci16_cf32_function_t fft_window_ci16_cf32_c(generic_opts_t cpu_cap, c
 
     SELECT_GENERIC_FN(fn, fname, tr_fft_window_ci16_cf32_generic, cpu_cap);
     SELECT_AVX2_FN(fn, fname, tr_fft_window_ci16_cf32_avx2, cpu_cap);
+    SELECT_AVX512BW_FN(fn, fname, tr_fft_window_ci16_cf32_avx512bw, cpu_cap);
 
     if (sfunc) *sfunc = fname;
     return fn;
